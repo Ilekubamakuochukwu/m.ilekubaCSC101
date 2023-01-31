9740cc72332fddc07b5b0156ced83e8a0dc467df
Ilekubamakuochukwu <115807052+Ilekubamakuochukwu@users.noreply.github.com>
2023-01-31 21:54:47 +0100

    Add files via upload

---
+++ M.ilekuba.rs/CA2.rs
@@ -0,0 +1,39 @@
+
+	fn main(){}
+	 fn GeoPo_Meger(){
+
+	//Array with the names of the commisioner
+	let arr1:[&str;5] = [ Aigbogun  Daudu,
+	Murtala Afeez bendu,
+	Okorocha Calistus Ogbonna,
+	Adewale Jimoh Akanbi,
+	Osazuwa Faith Etieye];
+	println!("\nArray with names of commisioners");
+	println!("array is {:?}",arr1.len());
+
+	
+	
+
+	//Array with Ministry
+	let arr2:[&str;5] = [Internal Affairs,
+	Justice,
+	Defence,
+	Power and steel,
+	Petroleum];
+	println!("\nArray with Ministry");
+	peintln!("array is {:?}",arr2);
+	println!("array size is :{}",arr2.len());
+	 
+	//Array with Geopolitical Zone
+	let arr3:[&str;5] = [ North East,
+	South South,
+	South West,
+	South east,
+Southy West
+];
+println!("\nArray with Geopolitical Zones");
+println!("array is {:?}",arr3);
+println!("array size is :{}",arr3.len());
+}
+	fn pub_Service() {
+		let mut 
